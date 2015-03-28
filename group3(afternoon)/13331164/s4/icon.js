9f9a780855370e3b766800548143b32b8fbac25f
cmsl3456 <kc1236317739@qq.com>
2015-03-28 16:29:27 +0800

    13331164
    13331164

---
+++ group3(afternoon)/13331164/s4/icon.js
@@ -0,0 +1,170 @@
+window.onload = function() {
+	circles = get_circles();
+	for (var i = 0; i < circles.length; i++)
+		circles[i].state = "noclick";
+	for (var i = 0; i < circles.length; i++) {
+		circles[i].onclick = function() {
+			if (this.state == "noclick" && IsClass(this, "enable")) {
+				click_circles(this);
+				var number;
+				var that = this;
+				Ajax("http://localhost:3000", function(number) {
+					getRandomNum(that, number);
+					stateChanging(that);
+					Check_Sum();
+				});
+			}
+		}
+	}
+	info_bar = document.getElementById('info-bar');
+	info_bar.onclick = function() {
+		if (IsClass(this, 'enable')) {
+			calculate_sum(this);
+		}
+	}
+    positioner = document.getElementById('bottom-positioner');
+ 	positioner.onmouseleave = function() {
+ 		reset();
+	}
+
+	var button = document.getElementById("icon");
+	button.onclick = function() {
+		var Arr1=[0,1,2,3,4];  
+		Arr1.sort(function(){return Math.random()>0.5?-1:1;});
+		var circles = get_circles();
+		robot(circles, Arr1, 0);
+	}
+}
+
+
+function get_circles() {
+	var ring = document.getElementById("control-ring-container");
+	var all_div = ring.getElementsByTagName('div');
+	var all_circles = [];
+	for (var i = 0; i < all_div.length; i++) {
+		if (i % 2 == 0)
+			all_circles.push(all_div[i]);
+	}
+	return all_circles;
+}
+
+function IsClass(elem, name) {
+	var reg = new RegExp(name);
+	return elem.className.match(reg);
+}
+
+function AddClass(elem, name) {
+	if (IsClass(elem, name))
+		return;
+	else
+		elem.className += ' ' + name;
+}
+
+function DeleteClass(elem, name) {
+	if (IsClass(elem, name))
+		elem.className=elem.className.replace(new RegExp(name), '');
+}
+
+function click_circles(elem) {
+	elem.getElementsByTagName('div')[0].style.visibility = "visible";
+	elem.state ="isclick";
+	var circles = get_circles();
+	for (var i = 0; i < circles.length; i++)
+		DeleteClass(circles[i], "enable");
+	AddClass(elem, "enable");
+}
+
+function getRandomNum(elem, number) {
+	elem.getElementsByTagName('div')[0].textContent = number;
+}
+
+function stateChanging(elem) {
+	var circles = get_circles();
+	DeleteClass(elem, 'enable');
+	for (var i = 0; i < circles.length; i++) {
+		if (circles[i].state == "noclick") {
+			AddClass(circles[i], "enable");
+		}
+	}
+}
+
+function Check_Sum() {
+	var circles = get_circles();
+	var flag = true;
+	for (var i = 0; i < circles.length; i++) {
+		if (circles[i].state != "isclick") {
+			flag = false;
+			break;
+		}
+	}
+	if (flag == true)
+		AddClass(document.getElementById('info-bar'), 'enable');
+}
+
+function calculate_sum(elem) {
+	var circles = get_circles();
+	var sum = 0;
+	for (var i = 0; i < circles.length; i++) {
+		sum += parseInt(circles[i].getElementsByTagName('div')[0].textContent);
+	}
+	elem.getElementsByTagName('span')[0].textContent = sum;
+	DeleteClass(elem, 'enable');
+}
+
+function reset() {
+	var circles = get_circles();
+	for (var i = 0; i < circles.length; i++) {
+		circles[i].state = "noclick";
+	    AddClass(circles[i], 'enable');
+	    circles[i].getElementsByTagName('div')[0].style.visibility = "hidden";
+	    circles[i].getElementsByTagName('div')[0].textContent = "...";
+	}
+	var info_bar = document.getElementById('info-bar');
+	info_bar.getElementsByTagName('span')[0].textContent = '';
+	DeleteClass(info_bar, 'enable');
+}
+
+function Ajax(url, callback) {
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+  	{// code for IE7+, Firefox, Chrome, Opera, Safari
+  		xmlhttp = new XMLHttpRequest();
+  	}
+	else
+  	{// code for IE6, IE5
+  		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  	}
+  	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+	xmlhttp.onreadystatechange = function() {
+  		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+    	callback(xmlhttp.responseText);
+    }
+  }
+}
+
+
+function robot(elem, RandomList, index) {
+	if (index < 5) {
+		if (elem[RandomList[index]].state == "isclick") {
+		index++;
+		robot(elem, RandomList, index);
+		return;
+		}
+		if (elem[RandomList[index]].state == "noclick" && IsClass(elem[RandomList[index]], "enable")) {
+			click_circles(elem[RandomList[index]]);
+			var number;
+			var that = elem[RandomList[index]];
+			Ajax("http://localhost:3000", function(number) {
+				getRandomNum(that, number);
+				stateChanging(that);
+				Check_Sum();
+				index++;
+				robot(elem, RandomList, index);
+			});
+		}
+	} else {
+		info_bar = document.getElementById('info-bar');
+		calculate_sum(info_bar);
+	}
+}
