a63e783bf9219c4dcb6abd95a36f65f0f2d29f05
VotorYe <iamgoodgay65@gmail.com>
2015-03-28 16:10:36 +0800

    comments

--- group2/13331313/s1/index.js
+++ group2/13331313/s1/index.js
@@ -2,15 +2,15 @@ window.onload = function() {
     var container = document.getElementById('at-plus-container');
     var info = document.getElementById('info-bar');
     var buttons = document.getElementById('control-ring').getElementsByTagName('li');
-    container.onmouseout = function(e) {
+    container.onmouseout = function(e) {            //鼠标移开后的操作
         if (e) {
             if (container.contains(e.relatedTarget)) return;
         } else if (event) {
             if (container.contains(event.toElement)) return;
         }
-        initial(buttons, info);
+        initial(buttons, info);             //复原所有
     }
-    container.onmouseover = function(e) {
+    container.onmouseover = function(e) {           //按钮展开后的初始化
         if (e) {
             if (container.contains(e.relatedTarget)) return;
         } else if (event) {
@@ -20,28 +20,28 @@ window.onload = function() {
     }
 }
 
-function initial(buttons, info) {
+function initial(buttons, info) {           //全局初始化
     for (var i = 0; i < buttons.length; i++) {
         (function(i) {buttonInitial(buttons, i)})(i);
     }
     info.getElementsByTagName('span')[0].innerHTML="";
 }
 
-function buttonInitial(buttons, p) {
+function buttonInitial(buttons, p) {            //按钮初始化
     var unread = buttons[p].getElementsByTagName('span')[1];
     unread.style.visibility = 'hidden';
     unread.innerHTML = "";
     buttons[p].style.backgroundColor = '#23479F';
 }
 
-function enableInterface(buttons, info) {
+function enableInterface(buttons, info) {           //为每个按钮添加onclick事件
     for (var i = 0; i < buttons.length; i++) {
         (function(i) {buttons[i].onclick = function(){buttonInterface(buttons, i);}})(i);
     }
     info.onclick = function() {sumUp(buttons, info);};
 }
 
-function buttonInterface(buttons, p) {
+function buttonInterface(buttons, p) {          //handler 
     var unread = buttons[p].getElementsByTagName('span')[1];
     var xmlhttp=new XMLHttpRequest()
     unread.style.visibility = 'visible';
@@ -53,12 +53,12 @@ function buttonInterface(buttons, p) {
             buttons[i].onclick = function(){};
         })(i)
     }
-    xmlhttp.onreadystatechange=function() {
+    xmlhttp.onreadystatechange=function() {             //得到服务器返回值后的操作
         if (xmlhttp.readyState==4 && xmlhttp.status==200) {
             unread.innerHTML = xmlhttp.responseText;
-            buttons[p].style.backgroundColor='gray';
+            buttons[p].style.backgroundColor='gray';        //灭活按钮
             buttons[p].onclick = function(){};
-            for (var i = 0; i < buttons.length; i++) {
+            for (var i = 0; i < buttons.length; i++) {          //激活其他按钮
                 if (buttons[i].getElementsByTagName('span')[1].innerHTML == "") {
                     (function(i) {
                         buttons[i].style.backgroundColor='#23479F';
@@ -72,10 +72,10 @@ function buttonInterface(buttons, p) {
     xmlhttp.send();
 }
 
-function sumUp(buttons, info) {
+function sumUp(buttons, info) {         //计算总和
     var sum = 0;
     var unread;
-    var allAva = true;
+    var allAva = true;                  //用于判断是否已经获得所有被加数
     for (var i = 0; i < buttons.length; i++) {
         unread = buttons[i].getElementsByTagName('span')[1];
         if (unread.innerHTML != "") {
--- group2/13331313/s4/index.js
+++ group2/13331313/s4/index.js
@@ -7,20 +7,20 @@ window.onload = function() {
     var container = document.getElementById('button');
     var info = document.getElementById('info-bar');
     var buttons = document.getElementById('control-ring').getElementsByTagName('li');
-    var ele = document.createElement('p');
+    var ele = document.createElement('p');          //点击顺序展示的ele
     var re = document.getElementById('bottom-positioner');
     document.getElementById('at-plus-container').insertBefore(ele, re);
     ele.style.position='absolute';
     ele.setAttribute('id', 'dis');
-    container.onmouseout = function(e) {
+    container.onmouseout = function(e) {            //鼠标移开后的操作
         if (e) {
             if (container.contains(e.relatedTarget)) return;
         } else if (event) {
             if (container.contains(event.toElement)) return;
         }
-        initial(buttons, info);
+        initial(buttons, info);             //复原所有
     }
-    container.onmouseover = function(e) {
+    container.onmouseover = function(e) {           //按钮展开后的初始化
         if (e) {
             if (container.contains(e.relatedTarget)) return;
         } else if (event) {
@@ -32,21 +32,21 @@ window.onload = function() {
     document.getElementById('apb').onclick = function() {arr = randomArray();simulate(buttons, 0);display(ele);}
 }
 
-function display(ele) {
+function display(ele) {             //点击顺序的展示
     ele.innerHTML = letters[arr[0]]+' '+letters[arr[1]]+' '+letters[arr[2]]+' '+letters[arr[3]]+' '+letters[arr[4]];
 }
 
-function randomsort(a, b) {
+function randomsort(a, b) {         //随机排序的比较函数
         return Math.random()>.5 ? -1 : 1; 
 }
 
-function randomArray() {
+function randomArray() {        //随机排列数组
     var a = [0, 1, 2, 3, 4];
     a = a.sort(randomsort);
     return a;
 }
 
-function simulate(buttons, p) {
+function simulate(buttons, p) {         //机器人模拟程序
     var i = 0;
     var event = document.createEvent('MouseEvents');
     event.initMouseEvent("click", true, true, window, 0, 0, 0, 0, false, false, false, false, 0, null);
@@ -54,7 +54,7 @@ function simulate(buttons, p) {
     buttons[arr[p]].onclick();
 }
 
-function initial(buttons, info) {
+function initial(buttons, info) {           //全局初始化
     for (var i = 0; i < buttons.length; i++) {
         (function(i) {buttonInitial(buttons, i)})(i);
     }
@@ -64,14 +64,14 @@ function initial(buttons, info) {
     document.getElementById('dis').innerHTML="";
 }
 
-function buttonInitial(buttons, p) {
+function buttonInitial(buttons, p) {            //按钮初始化
     var unread = buttons[p].getElementsByTagName('span')[1];
     unread.style.visibility = 'hidden';
     unread.innerHTML = "";
     buttons[p].style.backgroundColor = '#23479F';
 }
 
-function enableInterface(buttons, info) {
+function enableInterface(buttons, info) {           //为每个按钮添加onclick事件
     for (var i = 0; i < buttons.length; i++) {
         (function(i) {buttons[i].onclick = function(){return buttonInterface(buttons, i);}})(i);
     }
@@ -90,12 +90,12 @@ function buttonInterface(buttons, p) {
             buttons[i].onclick = function(){};
         })(i)
     }
-    xmlhttp.onreadystatechange=function() {
+    xmlhttp.onreadystatechange=function() {             //得到服务器返回值后的操作
         if (xmlhttp.readyState==4 && xmlhttp.status==200) {
             unread.innerHTML = xmlhttp.responseText;
-            buttons[p].style.backgroundColor='gray';
+            buttons[p].style.backgroundColor='gray';        //灭活按钮
             buttons[p].onclick = function(){};
-            for (var i = 0; i < buttons.length; i++) {
+            for (var i = 0; i < buttons.length; i++) {          //激活其他按钮
                 if (buttons[i].getElementsByTagName('span')[1].innerHTML == "") {
                     (function(i) {
                         buttons[i].style.backgroundColor='#23479F';
@@ -104,7 +104,7 @@ function buttonInterface(buttons, p) {
                 }
             }
             index++;
-            if (on == true) {
+            if (on == true) {           //当机器人程序启动时，继续自动操作
                 if (index < buttons.length) setTimeout(function() {buttons[arr[index]].onclick()}, 500);
                 else if (index == buttons.length) document.getElementById('info-bar').onclick();
                 else return;
@@ -115,10 +115,10 @@ function buttonInterface(buttons, p) {
     xmlhttp.send();
 }
 
-function sumUp(buttons, info) {
+function sumUp(buttons, info) {         //计算总和
     var sum = 0;
     var unread;
-    var allAva = true;
+    var allAva = true;                  //用于判断是否已经获得所有被加数
     for (var i = 0; i < buttons.length; i++) {
         unread = buttons[i].getElementsByTagName('span')[1];
         if (unread.innerHTML != "") {
